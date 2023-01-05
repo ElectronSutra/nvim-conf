17931d30838a5a2c6de7739daf35e97f8dca14d9
ElectronSutra <25828811+ElectronSutra@users.noreply.github.com>
2023-01-04 21:46:30 -0600

    🎨 Add colorscheme 'catppuccin'
    I have chosen to use Catppuccin because of its extensive support for
    3rd-party plugins (or as it calls them, "integrations"). This will make
    it MUCH easier than trying to style everything off of a single palette
    by hand.

---
+++ lua/conf/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	config = {
+		flavour = "frappe",
+		styles = {
+			comments = { "italic" },
+			conditionals = { "bold" },
+			keywords = { "bold" },
+		},
+	},
+}
