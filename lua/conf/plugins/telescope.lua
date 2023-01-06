a0612cad0a71e348dd144f9fe4462347f91c760c
ElectronSutra <25828811+ElectronSutra@users.noreply.github.com>
2023-01-06 11:37:23 -0600

    ✨ Add extension 'fzf' to Telescope
    REQ'D: Must independently install ripgrep for FZF to work properly.

--- lua/conf/plugins/telescope.lua
+++ lua/conf/plugins/telescope.lua
@@ -1,6 +1,22 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+					},
+				},
+			},
+		})
+		telescope.load_extension("fzf")
 	end,
 }
