840fbb594a5e806339a1a0308f5ca85bcf6c2959
ElectronSutra <25828811+ElectronSutra@users.noreply.github.com>
2023-01-06 12:25:30 -0600

    📦 Add plugin 'dressing'
    The plugin 'dressing' allows Telescope to act as the input and selector
    UI instead of the default Neovim selector/input ui. The telescope UI is
    much nicer. This configuration allows for visual selection instead of
    index-based selection and works for ANY plugin which uses the
    `vim.ui.select()` function for HUD elements -- that's most of them.

---
+++ lua/conf/plugins/dressing.lua
@@ -0,0 +1,12 @@
+return {
+	"stevearc/dressing.nvim",
+	config = {
+		input = {
+			enabled = true,
+		},
+		select = {
+			enabled = true,
+			backend = { "telescope" },
+		},
+	},
+}
---
+++ testing.lua
@@ -0,0 +1,15 @@
+vim.ui.select(
+{ "A", "B", "C", "D", "E", "F", "G" },
+{
+	prompt = "Select an option",
+	format_item = function(item)
+		return "Option " .. item
+	end,
+},
+function(item, idx)
+	if item then
+		print("You selected " .. item .. " at index " .. idx)
+	else
+		print("No selection.")
+	end
+end)
