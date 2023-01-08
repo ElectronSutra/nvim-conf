734a4397c25894c1c087bd4aae04341e2eb67e60
ElectronSutra <25828811+ElectronSutra@users.noreply.github.com>
2023-01-07 23:25:27 -0600

    📦 Add plugin 'noice'
    Also add fun plugin 'drop'. It adds a cute animation to the landing
    screen.

---
+++ lua/conf/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/noice.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+		"nvim-tree/nvim-web-devicons",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = {
+		cmdline = {
+			view = "cmdline",
+		},
+	},
+}
